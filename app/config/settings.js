f63c06fcddabf6d888df741f3f7163c281d6f212
Justice Johnson <justlyjohn198@gmail.com>
2023-08-13 21:32:20 +0100

    Added setttings for staging environment

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,20 +1,24 @@
-// import Constants from "expo-constants";
+import Constants from "expo-constants";
 
 const settings = {
   dev: {
     apiUrl: "http://192.168.0.164:2023/api/v1",
     imageUrl: "http://192.168.0.164:2023/uploads",
   },
+  staging: {
+    apiUrl: "http://192.168.0.164:2023/api/v1",
+    imageUrl: "http://192.168.0.164:2023/uploads",
+  },
   prod: {
-    apiUrl: "https://campaign-9ja-api.vercel.app/api/v1",
-    imageUrl: "https://campaign-9ja-api.vercel.app/uploads",
+    apiUrl: "https://campaign-9ja-app-production.up.railway.app/api/v1",
+    imageUrl: "https://campaign-9ja-app-production.up.railway.app/uploads",
   },
 };
 
 const getCurrentSettings = () => {
   if (__DEV__) return settings.dev;
 
-  // if (Constants.manifest.releaseChannel === "staging") return settings.staging;
+  if (Constants.manifest.releaseChannel === "staging") return settings.staging;
 
   return settings.prod;
 };
